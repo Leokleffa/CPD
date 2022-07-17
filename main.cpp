3bd7461f9dc484cda9fa08aecbecf361c4537cb4
Leokleffa <102036917+Leokleffa@users.noreply.github.com>
2022-07-17 17:35:37 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,10 +11,10 @@ int main()
     Estatisticas est;
     float tempo;
 
-    for(int tamanho=10; tamanho<41943041; tamanho*=2){
+    for(int tamanho=2; tamanho<20; tamanho*=2){
 
-//        dados.geraVetorAleatorio(tamanho,1,tamanho);
-        dados.geraVetorOrdenado(tamanho,1,tamanho);
+        dados.geraVetorAleatorio(tamanho,1,tamanho);
+//        dados.geraVetorOrdenado(tamanho,1,tamanho);
 //        dados.geraVetorQuaseOrdenado(tamanho,1,tamanho);
 //        dados.geraVetorReverso(tamanho,1,tamanho);
 
@@ -33,15 +33,17 @@ int main()
         tempo = timer.finalizaContagem();
 
         std::cout << " tempo " << tempo << " trocas " << est.trocas << " comparacoes " << est.comparacoes << std::endl;
-/*
+
         dados.imprime_registros();
 
         bool estaOrdenado = dados.checaSeRegistrosOrdenados();
         bool ordenacaoEstavel = dados.checaSeOrdenacaoFoiEstavel();
         std::cout << (estaOrdenado ? "Vetor ordenado" : "Vetor NAO ordenado") << " - "
                   << (ordenacaoEstavel ? "Ordenacao foi estavel" : "Ordenacao NAO foi estavel") << std::endl << std::endl;
-*/
+
     }
-    system("pause");
+
+    getchar();
+
     return 0;
 }
